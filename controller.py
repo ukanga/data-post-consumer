2141221de2456e85ed40041e2b3ba1d8655b615c
Ukang'a Dickson <ukanga@gmail.com>
2012-08-02 10:42:47 +0300

    simple rest interface

--- controller.py
+++ controller.py
@@ -1,11 +1,32 @@
 import cherrypy
+import json
 
 
 class FHConsume(object):
 
     @cherrypy.expose
     def index(self):
-        return "Consume Formhub data"
+        import pdb; pdb.set_trace()
+        if cherrypy.request.method not in ['POST']:
+            return u"Only POST requests are allowed."
+
+        content_type = cherrypy.request.headers['Content-type']
+        # get posted data
+        data = cherrypy.request.body.read()
+
+        # deal with json data
+        if content_type == 'application/json':
+            response = json.dumps({'status': 'OK'})
+            data = json.loads(data)
+        # deal with xml data
+        elif content_type == 'application/xml':
+            response = '<?xmlversion="1.0"?><data><status>OK</status></data>'
+        else:
+            response = "OK"
+
+        # TODO: do something or persist received data
+        cherrypy.response.headers['Content-type'] = content_type
+        return response
 
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0'})
